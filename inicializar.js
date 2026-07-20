f64da699eed7f34799f4a9a143280a1d462ae293
Miguel Angel <miguelad11c@gmail.com>
2026-07-19 23:51:00 -0500

    Add files via upload

---
+++ inicializar.js
@@ -0,0 +1,6 @@
+//inicializar tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// inicializar popovers
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
